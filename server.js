b8c35ab3d44d44302d2e3ba0932d464187c378b1
vishnudeviravi <ivishnudr@gmail.com>
2025-01-07 11:31:54 +0530

    ADDED DEV CHANGES

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/git-demo', (req, res) => {
   res.status(200).json({ message: 'git demo' });
 });
 
+app.get('/dev', (req, res) => {
+  res.status(200).json({ message: 'created at dev branch' });
+});
+
 app.listen(3000, () => {
   console.log('App is running');
 });
