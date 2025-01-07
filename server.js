31c4121bbe50fb33fabb601670e4cbb27c383273
vishnudeviravi <ivishnudr@gmail.com>
2025-01-07 10:45:58 +0530

    ADDED FIRST ROUTE

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/git-demo', (req, res) => {
+  res.status(200).json({ message: 'git demo' });
+});
+
 app.listen(3000, () => {
   console.log('App is running');
 });
